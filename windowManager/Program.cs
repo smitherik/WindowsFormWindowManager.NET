4bdddf5675e8fb8e5c2788f412c8e0779a9ce6af
smith.erik <smith.erik@protonmail.com>
2020-01-20 11:42:01 -0800

    Initial repo commit

---
+++ windowManager/Program.cs
@@ -0,0 +1,37 @@
+﻿/*------------------------------------------------------------------------------
+    Author     Erik Smith
+    Created    2020-01-20
+    Purpose    Control windows form applications such that at any point if there
+               are no longer any application windows open the application will 
+               close.
+-------------------------------------------------------------------------------
+    Modification History
+  
+    01/20/2020  Erik W. Smith
+    [30:34]
+        Initial Development
+        Single line change from the default template for a windows form 
+        application.
+-----------------------------------------------------------------------------*/
+
+
+using System;
+using System.Windows.Forms;
+
+namespace FormWindowManager
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //
+            //
+            Application.Run(new WindowManager());  // Using our window manager
+            //
+            //
+        }
+    }
+}
---
+++ windowManager/WindowManager.cs
@@ -0,0 +1,56 @@
+﻿/*------------------------------------------------------------------------------
+    Author     Erik Smith
+    Created    2020-01-20
+    Purpose    Control windows form applications such that at any point if there
+               are no longer any application windows open the application will 
+               close.
+-------------------------------------------------------------------------------
+    Modification History
+  
+    01/20/2020  Erik W. Smith
+    [1:eof]
+        Initial Development
+-----------------------------------------------------------------------------*/
+
+
+using System;
+using System.Windows.Forms;
+
+namespace FormWindowManager
+{
+    class WindowManager : ApplicationContext
+    {
+        //When each form closes, close the application if no other open forms
+        private void OnFormClosed(object sender, EventArgs e)
+        {
+            if (Application.OpenForms.Count == 0)
+            {
+                ExitThread();
+            }
+        }
+
+        //Any form which might be the last open form in the application should be created with this
+        public T CreateForm<T>() where T : Form, new()
+        {
+            var ret = new T();
+            ret.FormClosed += OnFormClosed;
+            return ret;
+        }
+
+        //I'm using Lazy here, because an exception is thrown if any Forms have been
+        //created before calling Application.SetCompatibleTextRenderingDefault(false)
+        //in the Program class
+        private static Lazy<WindowManager> _current = new Lazy<WindowManager>();
+        public static WindowManager Current => _current.Value;
+
+        //Startup forms should be created and shown in the constructor
+        public WindowManager()
+        {
+            var f1 = CreateForm<Form1>(); // default form created within the constructor
+            f1.Show();                    // yields modeless behavior
+            //f1.ShowDialog();               yields modal behavior
+
+        }
+
+    }
+}
